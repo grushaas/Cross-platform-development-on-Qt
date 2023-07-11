52a00a595afcfbbabce37c2514496b9b64e6125f
Grushin Maksim <grusha-as@yandex.ru>
2023-07-11 21:00:23 +0500

    Lesson 2 - Task 1 - Version 1.0

---
+++ build-The_main_Qt_modules-Desktop-Debug/CMakeFiles/The_main_Qt_modules_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/The_main_Qt_modules_autogen"
+  "The_main_Qt_modules_autogen/include/ui_mainwindow.h"
+  "The_main_Qt_modules_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/The_main_Qt_modules_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
